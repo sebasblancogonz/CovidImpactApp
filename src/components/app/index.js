55a9d69d546be6962f43343e6d0f851fdfb0872e
Sebastian Blanco <sebastianblancogonz@gmail.com>
2020-06-10 19:21:49 +0200

    :construction: Add infinite scroll to keep showing results

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,64 +1,87 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Platform, StyleSheet, View, ActivityIndicator } from 'react-native';
+import { Header, SearchBar, ListItem } from 'react-native-elements'
 import axios from 'axios'
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import { Header, SearchBar } from 'react-native-elements'
-import Municipalities from '../municipality/index'
+import { FlatList } from 'react-native-gesture-handler';
 
 
-export default class App extends Component {
+const App = () => {
 
-  state = {
-    search: '',
-    loading: false,
-    datasource: [],
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [dataPerPage, setDataPerPage] = useState(15)
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    const fecthData = async () => {
+      setLoading(true)
+      const res = await axios.get('https://spcovidimpact.herokuapp.com/api/data')
+      setData(res.data.data)
+      setLoading(false)
+    }
+
+    fecthData()
+  }, [])
+
+  const isLoading = () => {
+    if (loading) {
+      return (
+        <View style={[styleLoading.container, styleLoading.horizontal]}>
+          <ActivityIndicator size="large" color="#83878D" />
+        </View>
+      )
+    }
   }
 
-  updateSearch = (search) => {
-    this.setState({ search })
-    this.getResults(search)
+
+  const indexOfLastItem = currentPage * dataPerPage > data.length ? data.length : currentPage * dataPerPage;
+  const indexOfFirstItem = 0
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem)
+
+  const isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }) => {
+    const paddingToBottom = 0
+    return layoutMeasurement.height + contentOffset.y >=
+    contentSize.height - paddingToBottom;
   }
 
-
-  getResults = () => {
-    console.log(this.getUrl(this.state.search))
-    axios.get(this.getUrl(this.state.search))
-    .then(resp => {
-      console.log("on apppppp", resp.data)
-      this.setState({
-        loading: false,
-        datasource: resp.data,
-      })
-    }).catch((error) => {
-      console.log(error)
-    })
-}
-
-getUrl(search) {
-  return 'https://spcovidimpact.herokuapp.com/api/data'.concat(search != '' ? '?municipality=' + search : '')
-}
-
-  render() {
-    const { search, loading, datasource } = this.state;
-    return (
-      <View style={styles.container}>
-        <Header
-          placement="left"
-          backgroundColor="#F5F5F5"
-          leftComponent={{ icon: 'menu', color: 'black' }}
-          centerComponent={{ text: 'Covid Impact in Spain', style: { color: 'black' } }}
-          rightComponent={{ icon: 'home', color: 'black' }}
-        />
-        <SearchBar
-          platform={Platform.OS}
-          placeholder="Type municipality, province..."
-          onChangeText={this.updateSearch}
-          value={search}
-          showLoading={loading}
-        />
-      <Municipalities list={datasource} />
-      </View>
-    );
+  const fetchData = () => {
+    setCurrentPage(currentPage + 1)
   }
+
+  const renderRow = (({ item }) =>
+    <ListItem key={item.Cumum} title={item.Municip} subtitle={item.Province} />)
+
+  return (
+    <View style={styles.container}>
+      <Header
+        placement="left"
+        backgroundColor="#F5F5F5"
+        leftComponent={{ icon: 'menu', color: 'black' }}
+        centerComponent={{ text: 'Covid Impact in Spain', style: { color: 'black' } }}
+        rightComponent={{ icon: 'home', color: 'black' }}
+      />
+      <SearchBar
+        platform={Platform.OS}
+        placeholder="Type municipality, province..."
+        onChangeText={setSearch}
+        value={search}
+        showLoading={loading}
+      />
+      {isLoading()}
+      <FlatList
+        keyExtractor={(item, index) => index.toString()}
+        data={currentItems}
+        renderItem={renderRow}
+        onScroll={({ nativeEvent }) => {
+          if (isCloseToBottom(nativeEvent)) {
+            fetchData()
+          }
+        }}
+        scrollEventThrottle={400}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -77,3 +100,17 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+const styleLoading = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
+  }
+});
+
+export default App
